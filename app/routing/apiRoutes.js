b2f793c5da13ac6bf83b14750eca23116c20cea8
Goose <josephtrop@gmail.com>
2017-07-20 19:45:14 -0700

    updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,63 @@
 /**
  * Created by goosetaculous on 7/13/17.
  */
+
+var arr = [{
+    "name":"Rock",
+    "photo":"http://cdn-maf3.heartyhosting.com/sites/muscleandfitness.com/files/media/dwayne-johnson-rock-scream.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}]
+
+function computeDifference(dbScore,userScore){
+    let sum = 0
+    for(var i =0 ; i < dbScore.length; i++){
+        sum = sum + Math.abs(dbScore[i]- userScore[i])
+    }
+    return sum
+}
+
+
+function traverseUsers(userScore){
+    var compatible =""
+    if(arr.length > 0){
+        let diff=1000
+        arr.forEach((db)=>{
+            if(diff > computeDifference(db.scores,userScore)){
+                diff =  computeDifference(db.scores,userScore)
+                compatible = db
+            }
+        })
+    }
+    return compatible
+}
+
+module.exports = (app)=>{
+    app.get('/api/friends',(req,res)=>{
+        res.json(arr)
+    })
+    app.post('/api/friends',(req,res)=>{
+        var newScore = req.body.scores.map((x)=>{
+            return parseInt(x)
+        })
+        let obj={
+            name: req.body.name,
+            photo: req.body.photo,
+            scores : newScore
+        }
+        let userComp = traverseUsers(obj.scores)
+
+        res.send(userComp)
+        arr.push(obj)
+    })
+}
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,17 @@
 /**
  * Created by goosetaculous on 7/13/17.
  */
+var path = require("path");
+
+module.exports = (app)=>{
+
+    app.get('/survey',(req,res)=>{
+        res.sendFile(path.join(__dirname, '../public/survey.html'))
+
+    })
+
+    app.get('/',(req,res)=>{
+        res.sendFile(path.join(__dirname, '../public/home.html'))
+
+    })
+}
