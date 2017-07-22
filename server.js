c83984421296c3dbd651107427603919184f67c5
Goose <josephtrop@gmail.com>
2017-07-22 00:24:59 -0700

    updates

--- server.js
+++ server.js
@@ -8,7 +8,7 @@
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.text());
     app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'app/public')));
     require('./app/routing/htmlRoutes')(app)
     require('./app/routing/apiRoutes')(app)
     app.listen(PORT, function() {
