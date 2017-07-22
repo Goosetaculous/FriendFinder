7c56fb594c8934d423375bbb9cd5515dc118acf3
Goose <josephtrop@gmail.com>
2017-07-21 23:05:56 -0700

    test

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,21 @@
 /**
  * Created by goosetaculous on 7/13/17.
  */
+var arr =  [{
+    "name":"Rock",
+    "photo":"http://cdn-maf3.heartyhosting.com/sites/muscleandfitness.com/files/media/dwayne-johnson-rock-scream.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}]
+
+module.exports=arr
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,23 +1,7 @@
 /**
  * Created by goosetaculous on 7/13/17.
  */
-
-var arr = [{
-    "name":"Rock",
-    "photo":"http://cdn-maf3.heartyhosting.com/sites/muscleandfitness.com/files/media/dwayne-johnson-rock-scream.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-}]
+var arr = require('../data/friends')
 
 function computeDifference(dbScore,userScore){
     let sum = 0
